d1864e24a15f38018e47a4861213a61c58074849
guanjihuan <guanjihuan@163.com>
2022-09-09 16:04:42 +0800

    0.0.133

--- PyPI/src/guan/__init__.py
+++ PyPI/src/guan/__init__.py
@@ -2,7 +2,7 @@
 
 # With this package, you can calculate band structures, density of states, quantum transport and topological invariant of tight-binding models by invoking the functions you need. Other frequently used functions are also integrated in this package, such as file reading/writing, figure plotting, data processing.
 
-# The current version is guan-0.0.132, updated on September 08, 2022.
+# The current version is guan-0.0.133, updated on September 09, 2022.
 
 # Installation: pip install --upgrade guan
 
@@ -2548,16 +2548,16 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
         if os.path.isfile(filename1_with_path):
             if os.path.splitext(filename1)[1] not in banned_type:
                 if hide_file_type == None:
-                    f.write('+ '+str(filename1)+'\n')
+                    f.write('+ '+str(filename1)+'\n\n')
                 else:
-                    f.write('+ '+str(os.path.splitext(filename1)[0])+'\n')
+                    f.write('+ '+str(os.path.splitext(filename1)[0])+'\n\n')
         else:
             u0 += 1
             if divided_line != None and u0 != 1:
                 f.write('\n--------\n\n')
             if starting_from_h1 == None:
                 f.write('#')
-            f.write('# '+str(filename1)+'\n')
+            f.write('# '+str(filename1)+'\n\n')
 
             filenames2 = os.listdir(filename1_with_path) 
             i0 = 0     
@@ -2566,17 +2566,17 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                 if os.path.isfile(filename2_with_path):
                     if os.path.splitext(filename2)[1] not in banned_type:
                         if hide_file_type == None:
-                            f.write('+ '+str(filename2)+'\n')
+                            f.write('+ '+str(filename2)+'\n\n')
                         else:
-                            f.write('+ '+str(os.path.splitext(filename2)[0])+'\n')
+                            f.write('+ '+str(os.path.splitext(filename2)[0])+'\n\n')
                 else: 
                     i0 += 1
                     if starting_from_h1 == None:
                         f.write('#')
                     if show_second_number != None:
-                        f.write('## '+str(i0)+'. '+str(filename2)+'\n')
+                        f.write('## '+str(i0)+'. '+str(filename2)+'\n\n')
                     else:
-                        f.write('## '+str(filename2)+'\n')
+                        f.write('## '+str(filename2)+'\n\n')
                     
                     j0 = 0
                     filenames3 = os.listdir(filename2_with_path)
@@ -2585,17 +2585,17 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                         if os.path.isfile(filename3_with_path): 
                             if os.path.splitext(filename3)[1] not in banned_type:
                                 if hide_file_type == None:
-                                    f.write('+ '+str(filename3)+'\n')
+                                    f.write('+ '+str(filename3)+'\n\n')
                                 else:
-                                    f.write('+ '+str(os.path.splitext(filename3)[0])+'\n')
+                                    f.write('+ '+str(os.path.splitext(filename3)[0])+'\n\n')
                         else:
                             j0 += 1
                             if starting_from_h1 == None:
                                 f.write('#')
                             if show_third_number != None:
-                                f.write('### ('+str(j0)+') '+str(filename3)+'\n')
+                                f.write('### ('+str(j0)+') '+str(filename3)+'\n\n')
                             else:
-                                f.write('### '+str(filename3)+'\n')
+                                f.write('### '+str(filename3)+'\n\n')
 
                             filenames4 = os.listdir(filename3_with_path)
                             for filename4 in filenames4[::reverse_positive_or_negative]:
@@ -2603,13 +2603,13 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                                 if os.path.isfile(filename4_with_path):
                                     if os.path.splitext(filename4)[1] not in banned_type:
                                         if hide_file_type == None:
-                                            f.write('+ '+str(filename4)+'\n')
+                                            f.write('+ '+str(filename4)+'\n\n')
                                         else:
-                                            f.write('+ '+str(os.path.splitext(filename4)[0])+'\n')
+                                            f.write('+ '+str(os.path.splitext(filename4)[0])+'\n\n')
                                 else: 
                                     if starting_from_h1 == None:
                                         f.write('#')
-                                    f.write('#### '+str(filename4)+'\n')
+                                    f.write('#### '+str(filename4)+'\n\n')
 
                                     filenames5 = os.listdir(filename4_with_path)
                                     for filename5 in filenames5[::reverse_positive_or_negative]:
@@ -2617,13 +2617,13 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                                         if os.path.isfile(filename5_with_path): 
                                             if os.path.splitext(filename5)[1] not in banned_type:
                                                 if hide_file_type == None:
-                                                    f.write('+ '+str(filename5)+'\n')
+                                                    f.write('+ '+str(filename5)+'\n\n')
                                                 else:
-                                                    f.write('+ '+str(os.path.splitext(filename5)[0])+'\n')
+                                                    f.write('+ '+str(os.path.splitext(filename5)[0])+'\n\n')
                                         else:
                                             if starting_from_h1 == None:
                                                 f.write('#')
-                                            f.write('##### '+str(filename5)+'\n')
+                                            f.write('##### '+str(filename5)+'\n\n')
 
                                             filenames6 = os.listdir(filename5_with_path)
                                             for filename6 in filenames6[::reverse_positive_or_negative]:
@@ -2631,13 +2631,13 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                                                 if os.path.isfile(filename6_with_path): 
                                                     if os.path.splitext(filename6)[1] not in banned_type:
                                                         if hide_file_type == None:
-                                                            f.write('+ '+str(filename6)+'\n')
+                                                            f.write('+ '+str(filename6)+'\n\n')
                                                         else:
-                                                            f.write('+ '+str(os.path.splitext(filename6)[0])+'\n')
+                                                            f.write('+ '+str(os.path.splitext(filename6)[0])+'\n\n')
                                                 else:
                                                     if starting_from_h1 == None:
                                                         f.write('#')
-                                                    f.write('###### '+str(filename6)+'\n')
+                                                    f.write('###### '+str(filename6)+'\n\n')
     f.close()
 
 def move_all_files_to_root_directory(directory):
