0500442f429ba24f8cb3c46c60d188f0a45d0eb0
guanjihuan <guanjihuan@163.com>
2023-10-10 11:24:12 +0800

    update

--- Tutorial/division_of_parameter_array.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import guan
-
-cpus = 4
-parameter_array_all = np.arange(0, 17, 1) 
-for task_index in range(cpus):
-    parameter_array = guan.preprocess_for_parallel_calculations(parameter_array_all, cpus, task_index)
-    print(parameter_array)
-    print()
