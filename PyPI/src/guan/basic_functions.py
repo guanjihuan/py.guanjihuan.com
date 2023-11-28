74adc4faf864e43cc779a59f5bfe137c5b4835dc
guanjihuan <guanjihuan@163.com>
2023-11-29 01:06:26 +0800

    0.1.57

--- PyPI/src/guan/basic_functions.py
+++ PyPI/src/guan/basic_functions.py
@@ -7,7 +7,9 @@ def test():
     import guan
     current_version = guan.get_current_version('guan')
     print(f'Success in the installation of Guan package! The installed version is guan-{current_version}.')
+    print('Checking for the latest version of Guan package.')
     guan.notification_of_upgrade(timeout=5)
+    print('Check completed.')
 
 # 泡利矩阵
 @guan.function_decorator
